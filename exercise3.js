915d537d50ffc5080706a717d3d72c9e34110025
dennislimm <limdennis1405@gmail.com>
2023-07-26 21:48:54 +0700

    feat:exercise3

---
+++ exercise3.js
@@ -0,0 +1,36 @@
+console.log("Multiplication Table of 9");
+let n = 9
+let Multiples = 0
+for (i = 1; i<=n+1; i++){
+    console.log("9x",i,"=", n*i);
+}
+
+//Capitalizing the first letter of a word//
+
+
+
+
+//no.3 //
+console.log ("Converting cm to km");
+let cm = 500000;
+console.log ("cm = 500000");
+console.log ("converted to km=",cm/100000);
+
+//no.4 //
+console.log("mana yang lebih besar antara 50 dan 51")
+let number1 = 50;
+let number2 = 51;
+if(number1-number2 >= 1) {
+    console.log("lebih besar",number1, "daripada",number2);
+} else {
+    console.log("lebih besar",number2, "daripada",number1);
+}
+//Code to show type of an input//
+console.log("code to show the type of an input");
+console.log("Dennis");
+console.log(125);
+console.log(true);
+let string = "Dennis";
+let Numbers = 125;
+let Boolean = true;
+console.log("Dennis adalah", typeof string,",125 adalah", typeof Numbers,",true adalah", typeof Boolean);
